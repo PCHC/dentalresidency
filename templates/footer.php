c6e68fb45b9df7ec133559552729c1aec4069cf7
Chris Violette <cvioletterun@gmail.com>
2016-01-18 16:11:04 -0500

    Updated logo & a bit of styling

--- templates/footer.php
+++ templates/footer.php
@@ -4,8 +4,7 @@
   		&copy; <?php echo strftime('%Y'); ?> <?php bloginfo('name'); ?>
   	</div>
   	<div class="footer-locations">
-  		<em>Penobscot Pediatrics</em> | 1068 Union Street, Bangor, ME 04401 | <em>207-947-0147</em><br>
-  		<em>Helen Hunt Health Center</em> | 242 Brunswick St, Old Town, ME 04468 | <em>207-827-6128</em>
+  		<em>PCHC Dental Center</em> | 1048 Union Street, Bangor, ME 04401 | <em>207-992-2152</em>
   	</div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
